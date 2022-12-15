576274f17e63db05047b28b1378f1481c92bdfbd
iamstrawberry <realkoalaficationz@gmail.com>
2022-12-15 15:30:16 +0300

    Update linkextender.js

--- assets/js/linkextender.js
+++ assets/js/linkextender.js
@@ -6,7 +6,6 @@ function generate() {
         alert("Enter a URL first")
     } else {
         var chkurl = validURL(url)
-        console.log(chkurl)
         if (chkurl === false) {
             alert("Enter a valid URL\n\n" + url + " is not a valid URL\n\n\nA URL should contain the following:\n-protocol\n-domain name\n-port and path")
         } else {
@@ -39,7 +38,34 @@ function validURL(str) {
         '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
     return !!pattern.test(str);
 }
+console.log('Hello!\nDo sampleTest(url) for console generating')
 
+function sampleTest(url) {
+    var testurl = url
+    console.log("testurl: " + testurl)
+
+    if (testurl == '') {
+        console.log("Enter a URL first")
+    } else {
+        var chkurl = validURL(testurl)
+        console.log(chkurl)
+        if (chkurl === false) {
+            console.log("Enter a valid URL\n\n" + testurl + " is not a valid URL\n\n\nA URL should contain the following:\n-protocol\n-domain name\n-port and path")
+        } else {
+            const step1 = btoa(testurl);
+            let step2 = [];
+            for (const letter of step1.split("")) {
+                for (let i = 0; i < 4; i++) {
+                    step2.push(letter)
+                }
+            }
+            const finalUrl = btoa(step2.join(""));
+            currentURL = "https://thisisaverrrrrylonglinkextenderthatimadeforliterallynoreason.xwqx.cf/this/is/a/very/very/very/long-link/madeby/wq?thislinkissobigandlongandverycooolandnicetoo=" + encodeURIComponent(finalUrl);
+            console.log("Link: " + currentURL)
+            console.log("output: " + encodeURIComponent(finalUrl))
+        }
+    }
+}
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("generate").addEventListener("click", generate);
     document.getElementById("copy").addEventListener("click", copyLink);
