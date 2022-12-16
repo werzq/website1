988f497d6bbc3151bb669824e82d3b45ee34f7c1
iamstrawberry <realkoalaficationz@gmail.com>
2022-12-16 13:52:07 +0300

    e

--- assets/js/title.js
+++
@@ -1,2 +0,0 @@
-function scrolltitle() { document.title = title.substring(position, title.length) + title.substring(0, position), ++position > title.length && (position = 0), titleScroll = window.setTimeout(scrolltitle, 170) }
-title = "      Welcome      ", position = 0, scrolltitle();
