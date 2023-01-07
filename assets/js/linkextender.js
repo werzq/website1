e7e4486562c4aa1cddb9315b4e3feb61a55a06b1
iamstrawberry <realkoalaficationz@gmail.com>
2023-01-07 18:37:59 +0300

    updated link extender

--- assets/js/linkextender.js
+++ assets/js/linkextender.js
@@ -1,32 +1,34 @@
-let currentURL = "";
+const body = document.body
+const rbgValue = document.getElementById('rgbValue');
+const red = document.getElementById('rangeRed')
+const green = document.getElementById('rangeGreen')
+const blue = document.getElementById('rangeBlue')
 
-function generate() {
-    const url = document.getElementById("link").value;
-    if (url == '') {
-        alert("Enter a URL first")
-    } else {
-        var chkurl = validURL(url)
-        if (chkurl === false) {
-            alert("Enter a valid URL\n\n" + url + " is not a valid URL\n\n\nA URL should contain the following:\n-protocol\n-domain name\n-port and path")
-        } else {
-            const step1 = btoa(url);
-            let step2 = [];
-            for (const letter of step1.split("")) {
-                for (let i = 0; i < 4; i++) {
-                    step2.push(letter)
-                }
-            }
-            const finalUrl = btoa(step2.join(""));
-            currentURL = "https://thisisaverrrrrylonglinkextenderthatimadeforliterallynoreason.xwqx.cf/this/is/a/very/very/very/long-link/madeby/wq?thislinkissobigandlongandverycooolandnicetoo=" + encodeURIComponent(finalUrl);
-            document.getElementById("generated").style.display = "block";
-            document.getElementById("output").innerText = currentURL;
-        }
-    }
+const changeColor = () => {
+    let r = red.value;
+    let g = green.value;
+    let b = blue.value;
+    let bkgdClr = `rgb(${r},${g},${b})`
+    body.style.background = bkgdClr;
 }
 
-function copyLink() {
-    navigator.clipboard.writeText(currentURL);
-    alert("Copied to clipboard!");
+red.addEventListener('input', changeColor);
+green.addEventListener('input', changeColor);
+blue.addEventListener('input', changeColor)
+
+
+changeColor();
+
+function optSet() {
+    var x = document.getElementById("customiseset");
+    if (x.style.display === "none") {
+        document.getElementById("genurl").innerText = ''
+        x.style.display = "block";
+        document.getElementById('dropdnbuton').innerHTML = '<i class="fa-solid fa-caret-down"></i>'
+    } else {
+        x.style.display = "none";
+        document.getElementById('dropdnbuton').innerHTML = '<i class="fa-solid fa-caret-right"></i>'
+    }
 }
 
 function validURL(str) {
@@ -38,21 +40,24 @@ function validURL(str) {
         '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
     return !!pattern.test(str);
 }
-console.log('Hello!\nDo sampleTest(url) for console generating');
 
-function sampleTest(url) {
-    var testurl = url
-    console.log("testurl: " + testurl)
+function generateLink() {
+    url = document.getElementById("mainurl").value;
+    loadingtext = document.getElementById("loadingtxt").value;
+    bakcolor = body.style.background;
 
-    if (testurl == '') {
-        console.log("Enter a URL first")
+    if (url == '') {
+        alert("Enter a URL first")
     } else {
-        var chkurl = validURL(testurl)
-        console.log(chkurl)
+        var chkurl = validURL(url)
         if (chkurl === false) {
-            console.log("Enter a valid URL\n\n" + testurl + " is not a valid URL\n\n\nA URL should contain the following:\n-protocol\n-domain name\n-port and path")
+            alert("Enter a valid URL\n\n" + url + " is not a valid URL\n\n\nA URL should contain the following:\n-protocol\n-domain name\n-port and path")
         } else {
-            const step1 = btoa(testurl);
+            var dezk = document.getElementById("customiseset");
+            dezk.style.display = "none";
+            document.getElementById('dropdnbuton').innerHTML = '<i class="fa-solid fa-caret-right"></i>'
+
+            const step1 = btoa(url);
             let step2 = [];
             for (const letter of step1.split("")) {
                 for (let i = 0; i < 4; i++) {
@@ -60,13 +65,38 @@ function sampleTest(url) {
                 }
             }
             const finalUrl = btoa(step2.join(""));
-            currentURL = "https://thisisaverrrrrylonglinkextenderthatimadeforliterallynoreason.xwqx.cf/this/is/a/very/very/very/long-link/madeby/wq?thislinkissobigandlongandverycooolandnicetoo=" + encodeURIComponent(finalUrl);
-            console.log("Link: " + currentURL)
-            console.log("output: " + encodeURIComponent(finalUrl))
+            const urlpart = encodeURIComponent(finalUrl)
+            if (loadingtext == '') {
+                var ltxt = 'Redirecting...'
+            } else {
+                var ltxt = loadingtext
+            }
+
+            const step3 = btoa(ltxt);
+            let step4 = [];
+            for (const letter of step3.split("")) {
+                for (let i = 0; i < 4; i++) {
+                    step4.push(letter)
+                }
+            }
+            const finalltxt = btoa(step4.join(""));
+            const txtpart = encodeURIComponent(finalltxt)
+
+            const step5 = btoa(bakcolor);
+            let step6 = [];
+            for (const letter of step5.split("")) {
+                for (let i = 0; i < 4; i++) {
+                    step6.push(letter)
+                }
+            }
+
+            const finallcol = btoa(step6.join(""));
+            const colorpart = encodeURIComponent(finallcol)
+
+            currentURL = "https://thisisaverrrrrylonglinkextenderthatimadeforliterallynoreason.xwqx.cf/this/is/a/very/very/very/long-link/madeby/wq?sharing=" + urlpart + "&a=" + txtpart + "&link=" + colorpart;
+            console.log(url + ltxt + bakcolor);
+            document.getElementById("genurl").innerText = currentURL;
         }
     }
+
 }
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("generate").addEventListener("click", generate);
-    document.getElementById("copy").addEventListener("click", copyLink);
-});
