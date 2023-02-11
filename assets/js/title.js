79ea862b1e1dc9683d213ddbf65c079d0b26eaf8
iamstrawberry <realkoalaficationz@gmail.com>
2023-02-11 08:43:17 +0300

    clean

--- assets/js/title.js
+++
@@ -1,6 +0,0 @@
-var origTitle = document.title;
-
-function oldTitle() { document.title = origTitle }
-
-function newTitle() { document.title = "noo come back :(" }
-window.onblur = newTitle, window.onfocus = oldTitle
